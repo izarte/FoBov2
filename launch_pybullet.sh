d8b3654ebcc9a38c936292fb3efc293ae954004c
izarte <inigozarate01@gmail.com>
2024-03-30 03:29:41 +0100

    Add base model in xacro with urdf converter

---
+++ launch_pybullet.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 urdf/xacro.py -o urdf/fobo2.urdf urdf/fobo2.xacro
+python3 pybullet_test.py
--- pybullet_test.py
+++ pybullet_test.py
@@ -9,23 +9,23 @@ p.setGravity(0, 0, -9.81)
 planeId = p.loadURDF("plane.urdf", )
 startPos = [0,0,0.5]
 startOrientation = p.getQuaternionFromEuler([0,0,0])
-robot_id = p.loadURDF("fobo2.xacro",startPos, startOrientation)
+robot_id = p.loadURDF("urdf/fobo2.urdf",startPos, startOrientation)
 
 # for i in range(p.getNumJoints(bodyUniqueId = robot_id)):
 #     print(i)
 #     print(p.getJointInfo(bodyUniqueId = robot_id, jointIndex = i))
 
-while True:
-    p.stepSimulation()
-    time.sleep(1./240.)
+# while True:
+#     p.stepSimulation()
+#     time.sleep(1./240.)
 
 # #set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
-# for i in range (10000):
-#     p.getCameraImage(224, 224, renderer=p.ER_BULLET_HARDWARE_OPENGL)
-#     p.stepSimulation()
-#     p.setJointMotorControl2(bodyIndex=robot_id, jointIndex=2, controlMode=p.VELOCITY_CONTROL, targetVelocity=-50)
-#     p.setJointMotorControl2(bodyIndex=robot_id, jointIndex=3, controlMode=p.VELOCITY_CONTROL, targetVelocity=-50)
-#     time.sleep(1./240.)
+for i in range (10000):
+    # p.getCameraImage(224, 224, renderer=p.ER_BULLET_HARDWARE_OPENGL)
+    p.stepSimulation()
+    p.setJointMotorControl2(bodyIndex=robot_id, jointIndex=1, controlMode=p.VELOCITY_CONTROL, targetVelocity=2)
+    p.setJointMotorControl2(bodyIndex=robot_id, jointIndex=3, controlMode=p.VELOCITY_CONTROL, targetVelocity=-2)
+    time.sleep(1./240.)
 # cubePos, cubeOrn = p.getBasePositionAndOrientation(robot_id)
 # print(cubePos,cubeOrn)
 p.disconnect()
