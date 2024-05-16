7169bcf7e0852cddaeed9aca0449a6a32e5e9bec
inigo183 <inigozarate01@gmail.com>
2024-05-16 23:02:19 +0200

    Add efficient uart communication reading char by char

--- esp-control/src/main.cpp
+++ esp-control/src/main.cpp
@@ -3,49 +3,43 @@
 #include "pin_definitions.h"
 #include <HardwareSerial.h>
 
-
 Motor right_motor(RIGHT_PWM_PIN, RIGHT_PWM_CHANNEL, RIGHT_DIR_PIN, RIGHT_ENCODER_PIN_A, RIGHT_ENCODER_PIN_B, 0);
 Motor left_motor(LEFT_PWM_PIN, LEFT_PWM_CHANNEL, LEFT_DIR_PIN, LEFT_ENCODER_PIN_A, LEFT_ENCODER_PIN_B, 1);
 HardwareSerial UART1(2);
 
 
 void setup() {
+  Serial.begin(115200);
   right_motor.setup();
-  right_motor.set_speed(5);
+  right_motor.set_speed(0);
 
   left_motor.setup();
-  left_motor.set_speed(5);
+  left_motor.set_speed(0);
 
-  // Serial.begin(115200);  // Initialize debugging serial port
-  // Initialize UART1 on pins 16 (RX) and 17 (TX)
-  UART1.begin(115200, SERIAL_8N1, 16, 17); 
+  UART1.begin(1500000, SERIAL_8N1, 16, 17);
 }
 
-// UART managment variables
-std::string message_c_str;
-size_t split_pos;
-int left_speed = 0;
-int right_speed = 0;
-String encoders_speed_str;
+String inputBuffer = "";
+char readenChar;
+int separation_idx, left_speed, right_speed;
 
 void loop() {
-  // Check if data is available
-  if (UART1.available()) { 
-    String speeds_str = UART1.readString();
-    Serial.println("Speeds received: " + speeds_str);
-    // transform string to c++ string
-    message_c_str = speeds_str.c_str();
-    // Find the space separating the two numbers
-    split_pos = message_c_str.find(" ");
-    // Gather speeds from str message to int values
-    left_speed = static_cast<int>(std::stof(message_c_str.substr(0, split_pos)) * 100);
-    right_speed = static_cast<int>(std::stof(message_c_str.substr(split_pos + 1)) * 100);
+  while (UART1.available()) {
+    char readenChar = (char)UART1.read();
+    Serial.print(readenChar);
+    inputBuffer += readenChar;
+    if (readenChar == '\n') {
+      Serial.println("");
+      separation_idx = inputBuffer.indexOf(' ');
+      left_speed = inputBuffer.substring(0, separation_idx).toInt();
+      right_speed = inputBuffer.substring(separation_idx + 1).toInt();
 
-    left_motor.set_speed(left_speed);
-    right_motor.set_speed(right_speed);
-    encoders_speed_str =  String(left_motor.get_speed()) + " " +  String(right_motor.get_speed());
-    UART1.println(encoders_speed_str);
+      left_motor.set_speed(left_speed);
+      right_motor.set_speed(right_speed);
+
+      String encoders_speed_str = String(left_motor.get_speed()) + " " + String(right_motor.get_speed());
+      UART1.println(encoders_speed_str);
+      inputBuffer = "";
+    }
   }
-
-  delay(100);  // Delay for visibility
 }
